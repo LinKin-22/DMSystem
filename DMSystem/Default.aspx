e4563259dade0b7ab4bff4026cb3d30ef7e66bff
linkin-22 <shomnathcse22@gmail.com>
2018-09-11 11:18:36 +0600

    First commit

---
+++ DMSystem/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DMSystem._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h1>Dormitory Management System</h1>
+    <h3>Import file form Excel Sheet</h3>
+    
+    <div>
+        <table>
+            <tr>
+                <td>Select File : </td>
+                <td>
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+                <td>
+                    <asp:Button ID="btnImport" runat="server" Text="Import Data" OnClick="btnImport_Click" />
+                </td>
+            </tr>
+        </table>
+        
+        <div>
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Font-Bold="true"/>
+            <br />
+            <asp:GridView ID="gvData" runat="server" AutoGenerateColumns="False">
+                <EmptyDataTemplate>
+                    <div style="padding: 10px">
+                        Data not found!
+                    </div>
+                </EmptyDataTemplate>
+                <Columns>
+                    <asp:BoundField HeaderText="Student ID" DataField="StudentID"/>
+                    <asp:BoundField HeaderText="Student Roll" DataField="StudentRoll"/>
+                    <asp:BoundField HeaderText="Student Name" DataField="StudentName"/>
+                    <asp:BoundField HeaderText="Student Department" DataField="DepartmentName"/>
+                    <asp:BoundField HeaderText="Student Address" DataField="StudentAddress"/>
+                    <asp:BoundField HeaderText="Student Contact" DataField="ContactTitle"/>
+                    <asp:BoundField HeaderText="Student Email" DataField="StudentEmail"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+    
+</asp:Content>
---
+++ DMSystem/Default.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DMSystem
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateData();
+                lblMessage.Text = "Current Database data!";
+            }
+        }
+
+        private void PopulateData()
+        {
+            using (MuDatabaseEntities dc = new MuDatabaseEntities())
+            {
+                gvData.DataSource = dc.EmployeeMasters.ToList();
+                gvData.DataBind();
+            }
+        }
+
+        protected void btnImport_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.PostedFile.ContentType == "application/vnd.ms-excel" ||
+                FileUpload1.PostedFile.ContentType ==
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                try
+                {
+                    string fileName = Path.Combine(Server.MapPath("~/ImportDocument"),
+                        Guid.NewGuid().ToString() + Path.GetExtension(FileUpload1.PostedFile.FileName));
+                    FileUpload1.PostedFile.SaveAs(fileName);
+
+                    string conString = "";
+                    string ext = Path.GetExtension(FileUpload1.PostedFile.FileName);
+                    if (ext.ToLower() == ".xls")
+                    {
+                        conString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName +
+                                    ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;';";
+                    }
+                    else if (ext.ToLower() == ".xlsx")
+                    {
+                        conString = "Provider=Microsoft.ACE.OLEDB.12.0" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    "" +
+                                    ";Data Source=" + fileName +
+                                    ";Extended Properties='Excel 12.0 xml;HDR=Yes;IMEX=1;';";
+                    }
+
+                    string query =
+                        "Select [Student ID],[Student Roll],[Student Name],[Department Name],[Student Address],[Contact Title],[Student Email] from [EmployeeData$]";
+
+                    OleDbConnection con = new OleDbConnection(conString);
+                    if (con.State == System.Data.ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    OleDbCommand cmd = new OleDbCommand(query, con);
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    da.Dispose();
+                    con.Close();
+                    con.Dispose();
+
+
+                    // Import to Database
+                    using (MuDatabaseEntities dc = new MuDatabaseEntities())
+                    {
+                        foreach (DataRow dr in ds.Tables[0].Rows)
+                        {
+                            string stdID = dr["Student ID"].ToString();
+                            var v = dc.EmployeeMasters.Where(a => a.StudentID.Equals(stdID)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                //update here
+                                v.StudentRoll = dr["Student Roll"].ToString();
+                                v.StudentName = dr["Student Name"].ToString();
+                                v.DepartmentName = dr["Department Name"].ToString();
+                                v.StudentAddress = dr["Student Address"].ToString();
+                                v.ContactTitle = dr["Contact Title"].ToString();
+                                v.StudentEmail = dr["Student Email"].ToString();
+                            }
+                            else
+                            {
+                                // Insert
+                                dc.EmployeeMasters.Add(new EmployeeMaster
+                                {
+                                    StudentID = dr["Student ID"].ToString(),
+                                    StudentRoll = dr["Student Roll"].ToString(),
+                                    StudentName = dr["Student Name"].ToString(),
+                                    DepartmentName = dr["Department Name"].ToString(),
+                                    StudentAddress = dr["Student Address"].ToString(),
+                                    ContactTitle = dr["Contact Title"].ToString(),
+                                    StudentEmail = dr["Student Email"].ToString()
+                                });
+                            }
+                        }
+                        dc.SaveChanges();
+                    }
+                    PopulateData();
+                    lblMessage.Text = "Successfully data import done!!!";
+                }
+                
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
